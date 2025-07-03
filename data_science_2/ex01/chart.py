d0d3fc8a145c6dc0f2bd670eed093f16640a8530
MatLBS <135004599+MatLBS@users.noreply.github.com>
2025-07-03 18:29:07 +0200

    display nice graphs isquite hard

--- data_science_2/ex01/chart.py
+++ data_science_2/ex01/chart.py
@@ -3,14 +3,16 @@ import os
 import psycopg2
 from dotenv import load_dotenv
 import matplotlib.pyplot as plt
-import numpy as np
 import matplotlib.dates as mdates
 import seaborn as sns
+import datetime as dt
+import matplotlib.ticker as ticker
 
 load_dotenv()
 
+
 def draw_chart1(cur):
-    command_retrive_data = f"""
+    command_retrive_data = """
                 SELECT DATE(event_time) AS day,
                     COUNT(DISTINCT user_id) AS unique_users
                 FROM customers
@@ -35,17 +37,18 @@ def draw_chart1(cur):
     fmt = mdates.DateFormatter('%b')
     ax.xaxis.set_major_locator(locator)
     ax.xaxis.set_major_formatter(fmt)
-    
+
     ax.tick_params(axis='both', labelsize=7)
-    ax.set_ylabel('Number of customers', fontsize = 7)
+    ax.set_ylabel('Number of customers', fontsize=7)
+    ax.set_xlim([dt.date(2022, 10, 1), dt.date(2023, 2, 28)])
 
     plt.show()
 
 
 def draw_chart2(cur):
 
-    command_retrive_data = f"""
-                SELECT 
+    command_retrive_data = """
+                SELECT
                     EXTRACT(YEAR FROM event_time) AS year,
                     EXTRACT(MONTH FROM event_time) AS month,
                     SUM(price) AS total_value
@@ -54,17 +57,15 @@ def draw_chart2(cur):
                 GROUP BY year, month
                 ORDER BY year, month;
             """
-    
+
     cur.execute(command_retrive_data)
     results = cur.fetchall()
-    
+
     y = []
     x = []
     for result in results:
         x.append(str(result[0]) + "-" + str(result[1]))
-        y.append(result[2])
-        
-    print(y)
+        y.append(result[2] * 0.8)
 
     sns.set(style="darkgrid")
     fig, ax = plt.subplots()
@@ -73,20 +74,17 @@ def draw_chart2(cur):
     fmt = mdates.DateFormatter('%b')
     ax.xaxis.set_major_locator(locator)
     ax.xaxis.set_major_formatter(fmt)
-    
-    # plt.gcf().axes[0].yaxis.get_major_formatter().set_scientific(False)
 
     ax.tick_params(axis='both', labelsize=7)
-    ax.set_ylabel('Total sales in million of ₳', fontsize = 7)
-    # ax.set_ylim(0, 1500000)
-    ax.set_xlabel('month', fontsize = 7)
+    ax.set_ylabel('Total sales in million of ₳', fontsize=7)
+    ax.set_xlabel('month', fontsize=7)
 
     plt.show()
 
 
 def draw_chart3(cur):
 
-    command_retrive_data = f"""
+    command_retrive_data = """
                 SELECT DATE(event_time) AS day,
                     SUM(price) / COUNT(DISTINCT user_id)
                     AS average_spend
@@ -102,40 +100,47 @@ def draw_chart3(cur):
     y = []
     x = []
     for result in results:
-        y.append(result[1])
+        y.append(result[1] * 0.8)
         x.append(result[0])
 
     sns.set(style="darkgrid")
     fig, ax = plt.subplots()
-    ax.plot(x, y, linewidth=1)
+    ax.plot(x, y, linewidth=1, color='none')
+    ax.fill_between(x, y, alpha=0.5)
     locator = mdates.MonthLocator()
     fmt = mdates.DateFormatter('%b')
     ax.xaxis.set_major_locator(locator)
     ax.xaxis.set_major_formatter(fmt)
-    
+
     ax.tick_params(axis='both', labelsize=7)
-    ax.set_ylabel('Number of customers', fontsize = 7)
+    ax.set_ylabel('average spend/custoers in ₳', fontsize=7)
+
+    ax.set_xlim(dt.date(2022, 10, 1), dt.date(2023, 2, 28))
+
+    ax.set_ylim(0, 45)
+    ax.set_yticks(range(0, 46, 5))
 
     plt.show()
 
 
 def call_data():
-        conn = psycopg2.connect(
-          user=os.getenv('POSTGRES_LOGIN'),
-          password=os.getenv('POSTGRES_PASSWORD'),
-          host="localhost",
-          port="5432",
-          database=os.getenv('POSTGRES_NAME')
-        )
-        cur = conn.cursor()
-        
-        # draw_chart1(cur)
-        draw_chart2(cur)
-        # draw_chart3(cur)
+    conn = psycopg2.connect(
+        user=os.getenv('POSTGRES_LOGIN'),
+        password=os.getenv('POSTGRES_PASSWORD'),
+        host="localhost",
+        port="5432",
+        database=os.getenv('POSTGRES_NAME')
+    )
+    cur = conn.cursor()
 
-        conn.commit()
-        cur.close()
-        conn.close()
+    draw_chart1(cur)
+    draw_chart2(cur)
+    draw_chart3(cur)
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
 
 def main():
     try:
---
+++ data_science_2/ex02/mustache.py
@@ -0,0 +1,110 @@
+import pandas as pd
+import os
+import psycopg2
+from dotenv import load_dotenv
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import seaborn as sns
+import datetime as dt
+
+load_dotenv()
+
+
+def draw_first_charts(cur):
+    command_retrive_data = """
+                SELECT price
+                FROM customers
+                WHERE event_type = 'purchase'
+            """
+
+    cur.execute(command_retrive_data)
+    results = cur.fetchall()
+    results = [result[0] for result in results]
+
+    df = pd.DataFrame(results)
+
+    print(df.describe().apply(lambda s: s.apply('{0:.5f}'.format)))
+
+    sns.set(style="darkgrid")
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 6))
+
+    box = ax1.boxplot(results, vert=False, widths=0.8, patch_artist=True)
+    for patch in box['boxes']:
+        patch.set_facecolor('lightgreen')
+    for patch in box['medians']:
+        patch.set_color('black')
+    ax1.set_yticks([])
+    ax1.tick_params(axis='both', labelsize=7)
+    ax1.set_xlabel('price', fontsize=7)
+
+    box = ax2.boxplot(results, vert=False, widths=0.8, patch_artist=True, showfliers=False)
+    for patch in box['boxes']:
+        patch.set_facecolor('lightgreen')
+    for patch in box['medians']:
+        patch.set_color('black')
+    ax2.set_yticks([])
+    ax2.tick_params(axis='both', labelsize=7)
+    ax2.set_xlabel('price', fontsize=7)
+
+    ax2.set_xlim(-1, 13)
+    plt.show()
+
+
+def draw_chart3(cur):
+    command_retrive_data = """
+                SELECT user_id, AVG(price) AS avg_cart_price
+                FROM customers
+                WHERE event_type = 'cart'
+                GROUP BY user_id
+                HAVING AVG(price) BETWEEN 26 AND 43;
+            """
+
+    cur.execute(command_retrive_data)
+    results = cur.fetchall()
+    results = [result[1] for result in results]
+
+    sns.set(style="darkgrid")
+    fig, ax = plt.subplots()
+
+    box = ax.boxplot(results, vert=False, widths=0.8, patch_artist=True)
+
+
+    for patch in box['boxes']:
+        patch.set_facecolor('lightblue')
+
+    ax.set_yticks([])
+    ax.set_xticks(range(26, 43, 2))
+    ax.tick_params(axis='both', labelsize=7)
+    ax.set_xlabel('price', fontsize=7)
+
+
+    plt.show()
+
+
+def call_data():
+    conn = psycopg2.connect(
+        user=os.getenv('POSTGRES_LOGIN'),
+        password=os.getenv('POSTGRES_PASSWORD'),
+        host="localhost",
+        port="5432",
+        database=os.getenv('POSTGRES_NAME')
+    )
+    cur = conn.cursor()
+
+    # draw_first_charts(cur)
+    draw_chart3(cur)
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
+def main():
+    try:
+        call_data()
+    except AssertionError as error:
+        print(AssertionError.__name__ + ":", error)
+
+
+if __name__ == "__main__":
+    main()
